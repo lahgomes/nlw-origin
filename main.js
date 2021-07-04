7b07988a719c3cc00a5329ed44b6a4114c27b926
Larissa <larissa_gomes19@hotmail.com>
2021-07-04 18:44:50 -0300

    aula 04

--- main.js
+++ main.js
@@ -18,19 +18,18 @@ for(const link of links) {
 }
 
 //mudar o header da página quando der o scroll
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('header')
+  const navHeight = header.offsetHeight
 
-const header = document.querySelector('header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function() {
   if(window.scrollY >= navHeight) {
     //scroll é maior que a altura do header
     header.classList.add('scroll')
   } else {
     //menor que a altura do header
     header.classList.remove('scroll')
-  }
-})
+  }  
+}
 
 /*TESTIMONIALS CAROUSEL SLIDER SWIPER */
 const swiper = new Swiper('.swiper-container', {
@@ -57,6 +56,24 @@ scrollReveal.reveal(
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
   #contact .text, #contact .links  
+  #footer .lbrand, footer .social
   `, 
   { interval:100 }
   )
+
+/* BOTÃO VOLTAR PARA O TOPO */  
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 560) {
+     backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }        
+}
+
+/* WHEN SCROLL */   
+  window.addEventListener('scroll', function() {
+  changeHeaderWhenScroll()
+  backToTop()
+})
